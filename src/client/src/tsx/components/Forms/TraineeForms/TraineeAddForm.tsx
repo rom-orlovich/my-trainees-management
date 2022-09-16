ef050656521689c6ad581f47aa1f24b3e3cca889
rom <rom-orlovich@cyebr4s.dev>
2022-09-16 14:45:45 +0300

    delete the then method after the hundle submit of trainee in the client

--- src/client/src/tsx/components/Forms/TraineeForms/TraineeAddForm.tsx
+++ src/client/src/tsx/components/Forms/TraineeForms/TraineeAddForm.tsx
@@ -1,20 +1,14 @@
 import { traineesApi } from "../../../redux/api/hooksAPI";
-import { useAppSelector } from "../../../redux/hooks";
 
 import { addFunction } from "../../baseComponents/RHF-Components/FormsHook";
 import TraineeForm, { TraineeWithMemberProps } from "./TraineeForm";
 
 export function TraineeAddForm() {
-  const state = useAppSelector((state) => state.tablesPaginationState);
-
   const [addTrainee] = traineesApi.useCreateOneItemMutation();
   const handleSubmit = (body: TraineeWithMemberProps) => {
     addFunction({
       addItem: addTrainee,
-    })(body).then((value) => {
-      // memeberApi's methods will trigger only the data of memeberApi.
-      // Therefore the data of trainees will not refetch so, we need to trigger it manually.
-    });
+    })(body);
   };
 
   return <TraineeForm onSubmit={handleSubmit}></TraineeForm>;
