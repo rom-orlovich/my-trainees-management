d9edcb9297add76d57505cdb5b49b85f92e3e7b8
rom <rom-orlovich@cyebr4s.dev>
2022-09-24 15:33:39 +0300

    fix the style of settingsPage

--- src/client/src/tsx/pages/SettingsPage/SettingsPage.tsx
+++ src/client/src/tsx/pages/SettingsPage/SettingsPage.tsx
@@ -3,23 +3,23 @@ import { Link } from "react-router-dom";
 
 import MainRoute from "../../routes/MainRoute";
 import { APP_ROUTE } from "../../routes/routesConstants";
-
+import { genClassName } from "../../utilities/helpersFun";
+import style from "./SettingsPage.module.scss";
 function SettingsPage() {
   return (
     <MainRoute mainRoutes={APP_ROUTE.SETTINGS_ROUTE}>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          gap: "0.5rem",
-        }}
-      >
-        <Link to={APP_ROUTE.MUSCLES_GROUP_LIST_ROUTE}> Muscles Groups</Link>
-        <Link to={APP_ROUTE.LOCATION_ROUTE}> Locations</Link>
-        <Link to={APP_ROUTE.EXERCISES_LIST_ROUTE}> Exercises List</Link>
-        <Link to={APP_ROUTE.CITY_ROUTE}>Cities </Link>
-        <Link to={APP_ROUTE.EQUIPMENTS_LIST_ROUTE}>Equipment </Link>
-      </div>
+      <section className={genClassName(style.settings_page)}>
+        <div className={genClassName(style.settings_page_header)}>
+          <h1> Business Data </h1>
+        </div>
+        <div className={genClassName(style.settings_page_links)}>
+          <Link to={APP_ROUTE.MUSCLES_GROUP_LIST_ROUTE}> Muscles Groups</Link>
+          <Link to={APP_ROUTE.LOCATION_ROUTE}> Locations</Link>
+          <Link to={APP_ROUTE.EXERCISES_LIST_ROUTE}> Exercises List</Link>
+          <Link to={APP_ROUTE.CITY_ROUTE}>Cities </Link>
+          <Link to={APP_ROUTE.EQUIPMENTS_LIST_ROUTE}>Equipment </Link>
+        </div>
+      </section>
     </MainRoute>
   );
 }
