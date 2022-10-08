7517155e828d7509cf1c46e288ace699af24273f
rom <rom-orlovich@cyebr4s.dev>
2022-10-09 00:34:57 +0300

    fix no message after failure login

--- src/client/src/tsx/pages/SignInPage/LoginPage.tsx
+++ src/client/src/tsx/pages/SignInPage/LoginPage.tsx
@@ -20,8 +20,7 @@ function LoginPage() {
       .unwrap()
       .then(({ ...rest }) => {
         nav(`/${APP_ROUTE.HOME_PAGE}`);
-      })
-      .catch(console.log);
+      });
 
   return (
     <Card className={style.card_form}>
