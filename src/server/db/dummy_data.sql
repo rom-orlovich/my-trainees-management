51440ea20c476eccb7ad911120e54e8f92d83e28
Rom Orlovich <romorlovich@gmail.com>
2022-10-18 17:59:09 +0300

    restore the last dummy data file in server

--- src/server/db/dummy_data.sql
+++ src/server/db/dummy_data.sql
@@ -1,319 +1,54 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 14.5 (Ubuntu 14.5-1.pgdg20.04+1)
--- Dumped by pg_dump version 14.5 (Ubuntu 14.5-1.pgdg20.04+1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Data for Name: activities; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: alerts; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: cities; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO public.cities (city_id, city_name, district, population, user_id) VALUES (1, 'Kiryat Ono', 'center', 200000, 2);
-INSERT INTO public.cities (city_id, city_name, district, population, user_id) VALUES (2, 'Tel Aviv', 'center', 300000, 2);
-INSERT INTO public.cities (city_id, city_name, district, population, user_id) VALUES (3, 'Ramat Gan', 'center', 300000, 2);
-
-
---
--- Data for Name: locations; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO public.locations (location_id, city_id, street, user_id) VALUES (1, 1, 'Moshe Hass', 2);
-INSERT INTO public.locations (location_id, city_id, street, user_id) VALUES (2, 2, 'R.s', 2);
-INSERT INTO public.locations (location_id, city_id, street, user_id) VALUES (3, 3, 'Ben Gurion', 2);
-
-
---
--- Data for Name: providers; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: expenses; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: equipments; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO public.equipments (equipment_id, equipment_name, brand, manufacture_year, expense_id, user_id) VALUES (1, 'Dips bar', 'none', 2020, NULL, 2);
-INSERT INTO public.equipments (equipment_id, equipment_name, brand, manufacture_year, expense_id, user_id) VALUES (2, 'Olympic bar', 'none', 2020, NULL, 2);
-INSERT INTO public.equipments (equipment_id, equipment_name, brand, manufacture_year, expense_id, user_id) VALUES (3, 'Hand weight', 'none', 2020, NULL, 2);
-INSERT INTO public.equipments (equipment_id, equipment_name, brand, manufacture_year, expense_id, user_id) VALUES (4, 'Training mattress', 'none', 2020, NULL, 2);
-INSERT INTO public.equipments (equipment_id, equipment_name, brand, manufacture_year, expense_id, user_id) VALUES (5, 'Pull ups bar', 'none', 2020, NULL, 2);
-
-
---
--- Data for Name: muscles_group; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO public.muscles_group (muscles_group_id, muscles_group_name, user_id) VALUES (1, 'Legs', 2);
-INSERT INTO public.muscles_group (muscles_group_id, muscles_group_name, user_id) VALUES (2, 'Chest', 2);
-INSERT INTO public.muscles_group (muscles_group_id, muscles_group_name, user_id) VALUES (3, 'Back', 2);
-INSERT INTO public.muscles_group (muscles_group_id, muscles_group_name, user_id) VALUES (4, 'Triceps', 2);
-INSERT INTO public.muscles_group (muscles_group_id, muscles_group_name, user_id) VALUES (5, 'Shoulder', 2);
-INSERT INTO public.muscles_group (muscles_group_id, muscles_group_name, user_id) VALUES (6, 'ABS', 2);
-INSERT INTO public.muscles_group (muscles_group_id, muscles_group_name, user_id) VALUES (7, 'Biceps', 2);
-
-
---
--- Data for Name: exercises_list; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO public.exercises_list (exercise_id, exercise_name, muscles_group_id, equipment_id, user_id) VALUES (1, 'Chest Press', 2, 2, 2);
-INSERT INTO public.exercises_list (exercise_id, exercise_name, muscles_group_id, equipment_id, user_id) VALUES (2, 'Sqaut', 1, 2, 2);
-INSERT INTO public.exercises_list (exercise_id, exercise_name, muscles_group_id, equipment_id, user_id) VALUES (3, 'Lunge', 1, 3, 2);
-INSERT INTO public.exercises_list (exercise_id, exercise_name, muscles_group_id, equipment_id, user_id) VALUES (4, 'Dips', 2, 1, 2);
-
-
---
--- Data for Name: trainees; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO public.trainees (trainee_id, trainer_user_id, user_id, profile_id, sign_up_token) VALUES (1, 2, 3, 3, '');
-
-
---
--- Data for Name: incomes; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: leads; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: nutrition_programs_list; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: nutrition_program; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: schedule; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: participants_group; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: subscription_plans; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: training_programs_list; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: training_program; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: weeks; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Name: activities_activity_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.activities_activity_id_seq', 1, false);
-
-
---
--- Name: alerts_alert_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.alerts_alert_id_seq', 1, false);
-
-
---
--- Name: cities_city_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.cities_city_id_seq', 3, true);
-
-
---
--- Name: equipments_equipment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.equipments_equipment_id_seq', 5, true);
-
-
---
--- Name: exercises_list_exercise_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.exercises_list_exercise_id_seq', 4, true);
-
-
---
--- Name: expenses_expense_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.expenses_expense_id_seq', 1, false);
-
-
---
--- Name: incomes_income_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.incomes_income_id_seq', 1, false);
-
-
---
--- Name: leads_lead_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.leads_lead_id_seq', 1, false);
-
-
---
--- Name: locations_location_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.locations_location_id_seq', 3, true);
-
-
---
--- Name: muscles_group_muscles_group_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.muscles_group_muscles_group_id_seq', 7, true);
-
-
---
--- Name: nutrition_program_nutrition_program_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.nutrition_program_nutrition_program_id_seq', 1, false);
-
-
---
--- Name: nutrition_programs_list_nutrition_programs_list_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.nutrition_programs_list_nutrition_programs_list_id_seq', 1, false);
-
-
---
--- Name: participants_group_participant_groupe_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.participants_group_participant_groupe_id_seq', 1, false);
-
-
---
--- Name: profiles_profile_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.profiles_profile_id_seq', 3, true);
-
-
---
--- Name: providers_provider_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.providers_provider_id_seq', 1, false);
-
-
---
--- Name: schedule_schedule_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.schedule_schedule_id_seq', 1, false);
-
-
---
--- Name: subscription_plans_subscription_plan_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.subscription_plans_subscription_plan_id_seq', 1, false);
-
-
---
--- Name: trainees_trainee_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.trainees_trainee_id_seq', 1, true);
-
-
---
--- Name: training_program_training_program_row_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.training_program_training_program_row_id_seq', 1, false);
-
-
---
--- Name: training_programs_list_training_programs_list_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.training_programs_list_training_programs_list_id_seq', 1, false);
-
-
---
--- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.users_user_id_seq', 3, true);
-
-
---
--- Name: weeks_week_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.weeks_week_id_seq', 1, false);
-
-
---
--- PostgreSQL database dump complete
---
+INSERT INTO cities as c (population,district,city_name,user_id)
+   VALUES 
+(200000,'center','Kiryat Ono',2),
+(300000,'center','Tel Aviv', 2),
+(300000,'center','Ramat Gan', 2);
+
+
+INSERT INTO muscles_group as mg (user_id,muscles_group_name)
+   VALUES
+ (2,'Legs') ,
+ (2,'Chest') ,
+ (2,'Back') ,
+ (2,'Triceps') ,
+ (2,'Shoulder') ,
+ (2,'ABS') ,
+ (2,'Biceps') ;
+
+
+
+INSERT INTO equipments as eq
+ (manufacture_year,brand,equipment_name,user_id)
+   VALUES 
+(2020,'none', 'Dips bar',2) ,
+(2020,'none', 'Olympic bar',2) ,
+(2020,'none', 'Hand weight',2) ,
+(2020,'none', 'Training mattress',2),
+(2020,'none', 'Pull ups bar',2);
+
+
+INSERT INTO locations as lo (street,city_id,user_id)
+   VALUES 
+   ('Moshe Hass',1,2),
+    ('R.s',2,2) ,
+     ('Ben Gurion',3,2) ;
+  
+
+INSERT INTO exercises_list as exer (equipment_id,muscles_group_id,exercise_name,user_id)
+   VALUES 
+   (2,2,'Chest Press', 2) ,
+     (2,1,'Sqaut', 2) ,
+       (3,1,'Lunge', 2) ,
+       (1,2,'Dips', 2) ;
+
+
+update profiles set 
+ email='rom-orlovich@cyber4s.dev',status=true, date_join='2022-10-15T21:00:00.000Z',
+ location_id=1,phone_number='0543552144',birthday='2022-10-16T21:00:00.000Z',
+ identify_num='222222',gender='male',last_name='orlovich',first_name='rom'
+WHERE profile_id=3;
+
+ INSERT INTO trainees
+ (user_id,profile_id,trainer_user_id,sign_up_token)
+   VALUES (3,3, 2,'');
 
