d5cd55bb05b8f0e727f8c06a1b4b613a8916c7ec
Rom Orlovich <romorlovich@gmail.com>
2022-10-22 03:07:37 +0300

    add handle get statsic in the server

--- src/server/services/serviceStatistics/controllers/handleGetStatistic.ts
+++ src/server/services/serviceStatistics/controllers/handleGetStatistic.ts
@@ -1,7 +1,29 @@
+import eachDayOfInterval from "date-fns/eachDayOfInterval";
 import { RequestHandler } from "webpack-dev-server";
+import { TrainingProgramExerciseStatsAPI } from "../../../express";
+
+export const calLabelDates = (startDate: Date, endDate: Date) => {
+  const labels = eachDayOfInterval({ start: startDate, end: endDate });
+  const labelFormatted = labels.map((el) => el.toLocaleDateString());
+  return labelFormatted;
+};
+export const calIntensity = (
+  exerciseDataArr: TrainingProgramExerciseStatsAPI[]
+) => exerciseDataArr.map((el) => el.intensity);
 
 export const handleGetStatistic: RequestHandler = async (req, res, next) => {
-  if (!req.data_for_stats) return next();
+  if (!req.data_for_stats?.statsResult) return next();
   const { statsResult } = req.data_for_stats;
-  return res.status(200).json(statsResult);
+  if (statsResult.data.length > 0) {
+    const statsArr = statsResult.data;
+    const startDate = statsArr[0].update_date;
+    const endDate = statsArr[statsArr.length - 1].update_date;
+    const labelFormatted = calLabelDates(startDate, endDate);
+    const datasetsValues = calIntensity(statsArr);
+    return res
+      .status(200)
+      .json({ ...statsResult, labels: labelFormatted, datasetsValues });
+  }
+
+  res.status(200).json(statsResult);
 };
