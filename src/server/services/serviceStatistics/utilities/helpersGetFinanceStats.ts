761ad65139852d302615104e5f52f1b78af6681f
Rom Orlovich <romorlovich@gmail.com>
2022-11-08 23:50:56 +0200

    add normalize finance stats of yearly display in the server and add some comments in helpersGetFinanceStats.ts

--- src/server/services/serviceStatistics/utilities/helpersGetFinanceStats.ts
+++ src/server/services/serviceStatistics/utilities/helpersGetFinanceStats.ts
@@ -2,6 +2,7 @@ import {
   eachDayOfInterval,
   endOfWeek,
   format,
+  getDaysInMonth,
   lastDayOfMonth,
   startOfWeek,
 } from "date-fns";
@@ -9,72 +10,104 @@ import { formatDate } from "../../../utilities/helpers";
 
 import { ExpensesTableAPI, IncomesTableAPI } from "../serviceStatisticsTypes";
 
-const checkIfDateIsInWeeksRange = (date: Date) => {
-  const curDate = new Date(date);
-  const curDay = curDate.getDate();
-  const remainder = curDay % 7;
-  const reminder1 = 7 - remainder;
-
-  const start = formatDate(new Date(date.setDate(curDay - remainder)));
-  const end = formatDate(new Date(curDate.setDate(curDay + reminder1 - 1)));
-
-  const str = `${start}-${end}`;
-  return str;
-};
-const date1 = new Date();
-
-const createWeeksMonthFinanceObj = () => {
-  const curDate = new Date();
-  const firstDate = new Date(curDate.getFullYear(), curDate.getMonth(), 1);
-
-  const lastDate = lastDayOfMonth(curDate);
-  let weeksMonthObj = {} as Record<string, FinancesObj>;
-  const weeks = eachDayOfInterval(
-    { start: firstDate, end: lastDate },
-    { step: 7 }
-  );
-
-  for (let i = 0; i < weeks.length - 1; i++) {
-    const startWeek = formatDate(weeks[i], 0);
-    const endWeek = formatDate(weeks[i + 1], -1);
-    weeksMonthObj = {
-      ...weeksMonthObj,
-      [`${startWeek}-${endWeek}`]: { incomes: 0, expenses: 0 },
-    };
-  }
-
-  const endMonth = eachDayOfInterval({ start: weeks.at(-1)!, end: lastDate });
-  for (let i = 0; i < endMonth.length - 1; i++) {
-    const startWeek = formatDate(endMonth[i], 0);
-    const endWeek = formatDate(endMonth[i + 1], 0);
-    weeksMonthObj = {
-      ...weeksMonthObj,
-      [`${startWeek}-${endWeek}`]: { incomes: 0, expenses: 0 },
-    };
-  }
-  return weeksMonthObj;
-};
-
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
 interface FinancesObj {
   incomes: number;
   expenses: number;
 }
 const getNameMonth = (date: Date) => format(date, "MMMMMM");
+
+// Creates for each day in the week a financeObj.
+const createWeekDaysDisplay = () => {
+  const curDate = new Date();
+  // Get the start and the end date of the week.
+  const startWeek = startOfWeek(curDate);
+  const endWeek = endOfWeek(curDate);
+  let thisWeekDaysObj = {} as Record<string, FinancesObj>;
+  // Create array of dates.
+  eachDayOfInterval({
+    start: startWeek,
+    end: endWeek,
+  }).forEach((date) => {
+    thisWeekDaysObj = {
+      ...thisWeekDaysObj,
+      [formatDate(date, 0)]: { incomes: 0, expenses: 0 },
+    };
+  });
+  return thisWeekDaysObj;
+};
+
+// Gets date and return his week range in month.
+const getWeekRangeInMonthStr = (date: Date) => {
+  const curDate = new Date(date);
+  const curDay = curDate.getDate();
+  const remainder = curDay % 7;
+  const reminderDiff = 7 - remainder;
+
+  const start = formatDate(new Date(date.setDate(curDay - remainder)));
+  const end = formatDate(new Date(curDate.setDate(curDay + reminderDiff - 1)));
+
+  const str = `${start}-${end}`;
+  return str;
+};
+
+// Creates weeksRangeMonthFinanceObj.
+const createWeeksRangeMonthFinanceObj = () => {
+  const curDate = new Date();
+  const firstDate = new Date(curDate.getFullYear(), curDate.getMonth(), 1);
+  const lastDate = lastDayOfMonth(curDate);
+
+  let weeksRangeMonthFinanceObj = {} as Record<string, FinancesObj>;
+
+  // Creates weeksRangeInMonthFinanceObj by looping over the datesArr parameter the function get.
+  const loopToCreateWeeksRangeMonthFinanceObj = (
+    datesArr: Date[],
+    subtractLastDay = -1 // In the last days of month subtractLastDay is zero, to get all the days.
+  ) => {
+    for (let i = 0; i < datesArr.length - 1; i++) {
+      const startWeek = formatDate(datesArr[i], 0);
+      const endWeek = formatDate(datesArr[i + 1], subtractLastDay);
+      weeksRangeMonthFinanceObj = {
+        ...weeksRangeMonthFinanceObj,
+        [`${startWeek}-${endWeek}`]: { incomes: 0, expenses: 0 },
+      };
+    }
+  };
+
+  // Create dates array of the first day in each week that exist in month, start from the 1th.
+  const datesEachSevenDay = eachDayOfInterval(
+    { start: firstDate, end: lastDate },
+    { step: 7 }
+  );
+  loopToCreateWeeksRangeMonthFinanceObj(datesEachSevenDay);
+
+  // Create dates array from the last days of the month if they exist.
+  if (getDaysInMonth(curDate) > 28) {
+    const endMonthDates = eachDayOfInterval({
+      start: datesEachSevenDay.at(-1)!,
+      end: lastDate,
+    });
+    loopToCreateWeeksRangeMonthFinanceObj(endMonthDates, 0);
+  }
+
+  return weeksRangeMonthFinanceObj;
+};
+
+// Creates for each month a financeObj.
 const createMonthFinanceObj = () => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   let monthsFinancesObj = {} as Record<string, FinancesObj>;
   months.forEach((month) => {
     monthsFinancesObj = {
@@ -84,23 +117,8 @@ const createMonthFinanceObj = () => {
   });
   return monthsFinancesObj;
 };
-const createWeekDaysDisplay = () => {
-  const curDate = new Date();
-  const startWeek = startOfWeek(curDate);
-  const endWeek = endOfWeek(curDate);
-  let weekDaysObj = {} as Record<string, FinancesObj>;
-  eachDayOfInterval({
-    start: startWeek,
-    end: endWeek,
-  }).forEach((date) => {
-    weekDaysObj = {
-      ...weekDaysObj,
-      [date.toISOString()]: { incomes: 0, expenses: 0 },
-    };
-  });
-  return weekDaysObj;
-};
 
+// Calculates the finance sum as perspective of this week, weekly, monthly, yearly, and total.
 const calFinancesSum = (
   incomesData: IncomesTableAPI[],
   expenseData: ExpensesTableAPI[]
@@ -113,52 +131,98 @@ const calFinancesSum = (
     incomes: 0,
   };
 
-  const weeksRangeMonth = createWeeksMonthFinanceObj();
-  const weekDays = createWeekDaysDisplay();
+  let yearsFinanceObj = {} as Record<string, FinancesObj>;
+
+  const thisWeekDays = createWeekDaysDisplay();
+  const weeksRangeMonth = createWeeksRangeMonthFinanceObj();
   const monthsFinancesObj = createMonthFinanceObj();
   const curMonth = getNameMonth(new Date());
 
+  // Incomes
   incomesData.forEach((income) => {
-    incomesTotalSum += income.total_price;
+    const formattedDate = formatDate(income.date, 0);
+    const weekRangeInMonth = getWeekRangeInMonthStr(income.date);
     const dateMonth = getNameMonth(income.date);
-    const ISODate = income.date.toISOString();
-    const weeksRange = checkIfDateIsInWeeksRange(income.date);
-    if (dateMonth === curMonth) {
-      curMonthTotalSum.incomes += income.total_price;
-    }
-    if (monthsFinancesObj[dateMonth]) {
-      monthsFinancesObj[dateMonth].incomes += income.total_price;
-    }
-    if (weekDays[ISODate]) weekDays[ISODate].incomes += income.total_price;
+    const curYear = income.date.getFullYear();
+    incomesTotalSum += income.total_price;
+    // Calculate total sum.
+    if (dateMonth === curMonth) curMonthTotalSum.incomes += income.total_price;
 
-    if (weeksRangeMonth[weeksRange]) {
-      weeksRangeMonth[weeksRange].incomes += income.total_price;
+    // Calculate this week sum.
+    if (thisWeekDays[formattedDate])
+      thisWeekDays[formattedDate].incomes += income.total_price;
+
+    // Calculate weekly sum.
+    if (weeksRangeMonth[weekRangeInMonth])
+      weeksRangeMonth[weekRangeInMonth].incomes += income.total_price;
+
+    // Calculate monthly sum.
+    if (monthsFinancesObj[dateMonth])
+      monthsFinancesObj[dateMonth].incomes += income.total_price;
+
+    // Calculate year sum
+    const curYearFinance = yearsFinanceObj[curYear];
+    if (curYearFinance) {
+      curYearFinance.incomes += income.total_price;
+    } else {
+      // initial expense to 0 and set cur income.
+      yearsFinanceObj = {
+        ...yearsFinanceObj,
+        [curYear]: {
+          expenses: 0,
+          incomes: income.total_price,
+        },
+      };
     }
   });
+
+  // Expenses
   expenseData.forEach((expense) => {
+    const formattedDate = formatDate(expense.date, 0);
+    const weeksRange = getWeekRangeInMonthStr(expense.date);
     const dateMonth = getNameMonth(expense.date);
-    const ISODate = expense.date.toISOString();
-    const weeksRange = checkIfDateIsInWeeksRange(expense.date);
+    const curYear = expense.date.getFullYear();
     expensesTotalSum += expense.total_price;
-    if (dateMonth === curMonth) {
+    // Calculate total sum.
+    if (dateMonth === curMonth)
       curMonthTotalSum.expenses += expense.total_price;
-    }
-    if (monthsFinancesObj[dateMonth]) {
-      monthsFinancesObj[dateMonth].expenses += expense.total_price;
-    }
-    if (weekDays[ISODate]) weekDays[ISODate].expenses += expense.total_price;
-    if (weeksRangeMonth[weeksRange]) {
+
+    // Calculate this week sum.
+    if (thisWeekDays[formattedDate])
+      thisWeekDays[formattedDate].expenses += expense.total_price;
+
+    // Calculate monthly sum.
+    if (weeksRangeMonth[weeksRange])
       weeksRangeMonth[weeksRange].expenses += expense.total_price;
+
+    // Calculate year sum
+    if (monthsFinancesObj[dateMonth])
+      monthsFinancesObj[dateMonth].expenses += expense.total_price;
+
+    // Calculate year sum
+    const curYearFinance = yearsFinanceObj[curYear];
+    if (curYearFinance) {
+      curYearFinance.expenses += expense.total_price;
+    } else {
+      // initial expense to 0 and set cur income.
+      yearsFinanceObj = {
+        ...yearsFinanceObj,
+        [curYear]: {
+          incomes: 0,
+          expenses: expense.total_price,
+        },
+      };
     }
   });
 
   return {
     expensesTotalSum,
     incomesTotalSum,
+    thisWeekDays,
     curMonthTotalSum,
-    monthsFinancesObj,
-    weekDays,
     weeksRangeMonth,
+    monthsFinancesObj,
+    yearsFinanceObj,
   };
 };
 
