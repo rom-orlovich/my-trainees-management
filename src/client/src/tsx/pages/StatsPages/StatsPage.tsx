2d77b09eaa59a678b0681411b94bfbe8f25e14a3
Rom Orlovich <romorlovich@gmail.com>
2022-11-13 18:58:28 +0200

    before create generic stats page

---
+++ src/client/src/tsx/pages/StatsPages/StatsPage.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode } from "react";
+import { PropsBasic } from "../../components/baseComponents/baseComponentsTypes";
+import { InputLabel } from "../../components/baseComponents/RHF-Components/InputLabel/InputLabel";
+import { SelectInput } from "../../components/baseComponents/RHF-Components/SelectInput/SelectInput";
+import useOnChangeInput from "../../hooks/useOnChangeInput";
+import { GenericRecord } from "../../types";
+import style from "./StatsPages.module.scss";
+
+function StatsPage({
+  children,
+}: PropsBasic & { children: (data: { gt: string; lt: string }) => ReactNode }) {
+  const [{ gt, lt }, onChange] = useOnChangeInput({
+    gt: "",
+    lt: "",
+  });
+
+  return (
+    <section className={style.stats_page}>
+      <div className={style.stats_page_header_filters}>
+        <span className={style.dates_container}>
+          <InputLabel
+            LabelProps={{ labelText: "Date Start", htmlFor: "gt" }}
+            InputProps={{ type: "date", onChange }}
+          />
+          <InputLabel
+            LabelProps={{ labelText: "Date End", htmlFor: "lt" }}
+            InputProps={{ type: "date", onChange }}
+          />
+        </span>
+      </div>
+
+      <div className={style.stats_page_container}>
+        {typeof children === "function"
+          ? children({
+              gt: "",
+              lt: "",
+            })
+          : children}
+      </div>
+    </section>
+  );
+}
+
+export default StatsPage;
--- src/client/src/tsx/pages/StatsPages/TraineeStatsPage/TraineeStatsPage.tsx
+++ src/client/src/tsx/pages/StatsPages/TraineeStatsPage/TraineeStatsPage.tsx
@@ -1,16 +1,46 @@
 import React from "react";
 import Card from "../../../components/baseComponents/Card/Card";
-import InsteadOutletRoutes from "../../../routes/utilities/InsteadOutletRoutes";
+import LoadingSpinner from "../../../components/baseComponents/LoadingSpinner/LoadingSpinner";
+import useGetUserLoginData from "../../../hooks/useGetUserLoginData";
+import { traineesApi } from "../../../redux/api/hooksAPI";
+import { GetCitiesGendersAgesStatsAPI } from "../../../redux/api/interfaceAPI";
+
 import StatsPageStyle from "../StatsPages.module.scss";
 
 function TraineeStatsPage() {
+  const { user_id } = useGetUserLoginData();
+  const { data, isLoading, isFetching, isError } = traineesApi.useGetItemsQuery(
+    {
+      userID: user_id,
+      displayStats: "all",
+    }
+  );
+
   return (
     <section className={StatsPageStyle.stats_page}>
       <div className={StatsPageStyle.stats_page_container}>
-        <Card className={StatsPageStyle.distribution_card_container}>HEY </Card>
-        <Card className={StatsPageStyle.distribution_card_container}> </Card>
-        <Card className={StatsPageStyle.distribution_card_container}> </Card>
-        <Card className={StatsPageStyle.chart_card_container}></Card>
+        <LoadingSpinner stateData={{ data, isLoading, isFetching, isError }}>
+          {(data) => {
+            const Data = data as unknown as {
+              stats: GetCitiesGendersAgesStatsAPI;
+            };
+            console.log(Data);
+            return (
+              <>
+                <Card
+                  className={StatsPageStyle.distribution_card_container}
+                ></Card>
+                <Card
+                  className={StatsPageStyle.distribution_card_container}
+                ></Card>
+                <Card
+                  className={StatsPageStyle.distribution_card_container}
+                ></Card>
+                <Card className={StatsPageStyle.chart_card_container}></Card>
+              </>
+            );
+          }}
+        </LoadingSpinner>
       </div>
     </section>
   );
