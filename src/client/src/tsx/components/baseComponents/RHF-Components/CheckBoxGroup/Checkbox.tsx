0cdc1f4ffae081b2b104ce300be7bd08c50f39f0
Rom Orlovich <romorlovich@gmail.com>
2022-12-01 21:05:15 +0200

    create checkboxs group as list component

--- src/client/src/tsx/components/baseComponents/RHF-Components/CheckBoxGroup/Checkbox.tsx
+++ src/client/src/tsx/components/baseComponents/RHF-Components/CheckBoxGroup/Checkbox.tsx
@@ -1,26 +1,25 @@
 import React from "react";
 import { genClassName } from "../../../../utilities/helpersFun";
-import { InputLabel } from "../InputLabel/InputLabel";
+import { LiProps } from "../../baseComponentsTypes";
+import { InputLabel, InputProps, LabelProps } from "../InputLabel/InputLabel";
+
 import style from "./CheckboxGroup.module.scss";
 
-interface CheckboxLabelProps {
-  InputProps: React.DetailedHTMLProps<
-    React.InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  > & { isError?: boolean };
-  LabelProps: React.DetailedHTMLProps<
-    React.LabelHTMLAttributes<HTMLLabelElement>,
-    HTMLLabelElement
-  > & { labelText: string };
-  children?: React.ReactNode;
+export interface CheckBox {
+  label: string;
+  id: string;
+  InputProps?: InputProps;
+  LabelProps: LabelProps;
 }
-
-function Checkbox({ InputProps, LabelProps }: CheckboxLabelProps) {
+function Checkbox({
+  InputProps,
+  LabelProps,
+}: CheckBox & { liProps?: LiProps; index: number }) {
   return (
     <InputLabel
       LabelProps={{
         ...LabelProps,
-        className: genClassName(style.checkbox_label, LabelProps.className),
+        className: genClassName(style.checkbox_label, LabelProps?.className),
       }}
       InputProps={{ ...InputProps, type: "checkbox" }}
     />
--- src/client/src/tsx/components/baseComponents/RHF-Components/CheckBoxGroup/CheckboxGroup.tsx
+++ src/client/src/tsx/components/baseComponents/RHF-Components/CheckBoxGroup/CheckboxGroup.tsx
@@ -1,19 +1,28 @@
 import React from "react";
-import { InputLabel, InputProps, LabelProps } from "../InputLabel/InputLabel";
+import { genClassName } from "../../../../utilities/helpersFun";
+import List from "../../List/List";
+
+import Checkbox, { CheckBox } from "./Checkbox";
 import style from "./CheckboxGroup.module.scss";
 
-export interface CheckBox {
-  label: string;
-  id: string;
-  inputProps?: InputProps;
-  labelProps?: LabelProps;
-}
 export interface CheckBoxGroupProps {
   checkboxDataArr: CheckBox[];
+  groupName: string;
 }
 
-function CheckBoxGroup({ checkboxDataArr }: CheckBoxGroupProps) {
-  return <>{checkboxDataArr.map((checkboxData) => {})} </>;
+function CheckBoxGroup({ checkboxDataArr, groupName }: CheckBoxGroupProps) {
+  return (
+    <List
+      className={genClassName(style.checkbox_group_container)}
+      LI={(props) => {
+        const inputProps = props.InputProps
+          ? { ...props.InputProps, name: groupName }
+          : { name: groupName };
+        return <Checkbox {...props} InputProps={inputProps} />;
+      }}
+      dataArr={checkboxDataArr}
+    />
+  );
 }
 
 export default CheckBoxGroup;
