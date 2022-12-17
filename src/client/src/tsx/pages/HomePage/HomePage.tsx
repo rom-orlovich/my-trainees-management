77cd26810500dce456f2aba6fc36f83373e5c742
Rom Orlovich <romorlovich@gmail.com>
2022-12-17 17:07:43 +0200

    delete unused lines in HomePage

--- src/client/src/tsx/pages/HomePage/HomePage.tsx
+++ src/client/src/tsx/pages/HomePage/HomePage.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Navigate, NavLink, Outlet, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import InsteadOutletRoutes from "../../routes/utilities/InsteadOutletRoutes";
 import { APP_ROUTE } from "../../routes/appRoutesConstants";
 import style from "./HomePage.module.scss";
 
 function HomePage() {
-  const { pathname } = useLocation();
   return (
     <section className={style.homePage_container}>
       <InsteadOutletRoutes InsteadOutletRoutesPaths={""}>
