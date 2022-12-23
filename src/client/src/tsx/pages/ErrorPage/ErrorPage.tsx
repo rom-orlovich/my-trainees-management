fccbcd44ca41992935d2c75347b0e1891bd1b706
Rom Orlovich <romorlovich@gmail.com>
2022-12-23 18:27:31 +0200

    set the link to load after the video is loading in errorPage

--- src/client/src/tsx/pages/ErrorPage/ErrorPage.tsx
+++ src/client/src/tsx/pages/ErrorPage/ErrorPage.tsx
@@ -1,20 +1,24 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import BackgroundVideo from "../../components/baseComponents/BackgroundVideo/BackgroundVideo";
-import useLoadVideo from "../../hooks/useLoadVideo";
 import { genClassName } from "../../utilities/helpersFun";
 import homePageStyle from "../HomePage/HomePage.module.scss";
 
 function ErrorPage() {
+  const [loadingState, setLoadingState] = useState(true);
   return (
     <div
       className={genClassName(homePageStyle.home_page_container, "error_page")}
     >
-      <BackgroundVideo />
+      <BackgroundVideo
+        onLoadedData={() => {
+          setLoadingState(false);
+        }}
+      />
       <div className={homePageStyle.home_page_text}>
         <>
           <h1> Oops, Something Is Went Wrong!</h1>
-          {<NavLink to={-1 as any}> Previous Page</NavLink>}
+          {!loadingState && <Link to={-1 as any}> Previous Page</Link>}
         </>
       </div>
     </div>
--- src/client/src/tsx/pages/HomePage/HomePage.tsx
+++ src/client/src/tsx/pages/HomePage/HomePage.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import InsteadOutletRoutes from "../../routes/utilities/InsteadOutletRoutes";
 import { APP_ROUTE } from "../../routes/appRoutesConstants";
 import style from "./HomePage.module.scss";
 import BackgroundVideo from "../../components/baseComponents/BackgroundVideo/BackgroundVideo";
-import useLoadVideo from "../../hooks/useLoadVideo";
 
 function HomePage() {
   return (
@@ -15,7 +14,7 @@ function HomePage() {
         <div className={style.home_page_text}>
           <>
             <h1> Your Business Is Our Pleasure</h1>
-            {<NavLink to={`/${APP_ROUTE.LOGIN_ROUTE}`}> Get Started</NavLink>}
+            {<Link to={`/${APP_ROUTE.LOGIN_ROUTE}`}> Get Started</Link>}
           </>
         </div>
       </InsteadOutletRoutes>
