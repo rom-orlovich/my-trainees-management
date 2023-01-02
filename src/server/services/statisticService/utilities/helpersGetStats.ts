c339849e3e8029a5a45946ef25f69f91f4b5e0df
Rom Orlovich <romorlovich@gmail.com>
2023-01-02 18:23:30 +0200

    delete unnecessary lines of code

--- src/server/services/statisticService/utilities/helpersGetStats.ts
+++ src/server/services/statisticService/utilities/helpersGetStats.ts
@@ -270,7 +270,6 @@ export const calAllTimeLineObj = <T extends GenericRecord<any>>(
   const formattedDate = getDateLocal(date);
 
   const weekRangeInMonth = getWeekRangeInMonthStr(date);
-
   const dateMonth = getNameMonth(date);
   const curYear = date.getFullYear();
 
